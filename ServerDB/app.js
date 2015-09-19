130dec0be6b70bac81ae238b77d294bceae86a07
tensortrash <tayebzaidi@gmail.com>
2015-09-19 11:40:47 -0500

    change launch and app

--- ServerDB/app.js
+++ ServerDB/app.js
@@ -1,10 +1,10 @@
-console.log("Hello!")
+console.log("Hello!");
 var http = require('http');
 http.createServer(function (request, response) {
 	response.writeHead(200, {
 		'Content-Type': 'text/plain'
-	})
-	response.write('Message Received!')
-	response.end()
+	});
+	response.write('Message Received!');
+	response.end();
 }).listen(8080);
 
